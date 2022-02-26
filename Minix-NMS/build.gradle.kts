d3dcc04da4700dfff6d7f6bf1db903307c867cf6
Racci <racci@racci.dev>
2022-02-26 21:02:17 +1100

    fix: Revert NMS stuff because i guess it actually messes with the compiler lol so that was for nothing
    Signed-off-by: Racci <racci@racci.dev>

--- Minix-NMS/build.gradle.kts
+++ Minix-NMS/build.gradle.kts
@@ -3,31 +3,10 @@ plugins {
     id("dev.racci.minix.nms")
     id("dev.racci.minix.purpurmc")
     kotlin("plugin.serialization")
-    `maven-publish`
+    id("dev.racci.minix.publication")
 }
 
 dependencies {
     api(project(":"))
     compileOnly(libs.kotlinx.serialization.json)
-}
-
-publishing {
-    repositories {
-        maven("https://repo.racci.dev/releases") {
-            name = "RacciRepo"
-            credentials(PasswordCredentials::class)
-        }
-    }
-    publications {
-        register("maven", MavenPublication::class) {
-            artifact(tasks.reobfJar.get().outputJar) {
-                classifier = null
-                builtBy(tasks.reobfJar)
-            }
-            artifact(tasks.kotlinSourcesJar) {
-                classifier = "sources"
-                builtBy(tasks.kotlinSourcesJar)
-            }
-        }
-    }
 }
