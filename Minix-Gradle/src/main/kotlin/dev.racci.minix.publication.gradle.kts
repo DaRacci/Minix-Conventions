4b152d22e8a8a23f03d8e83a24875719b5418c74
Racci <racci@racci.dev>
2022-02-26 19:31:05 +1100

    chore: Revert NMS and publication changes, they will need a actual gradle plugin to do
    Signed-off-by: Racci <racci@racci.dev>

--- Minix-Gradle/src/main/kotlin/dev.racci.minix.publication.gradle.kts
+++ Minix-Gradle/src/main/kotlin/dev.racci.minix.publication.gradle.kts
@@ -4,6 +4,16 @@ plugins {
     id("org.jetbrains.dokka")
 }
 
+project.beforeEvaluate {
+    val lib = configurations.create("lib")
+    plugins.withType<JavaPlugin> {
+        extensions.getByType<SourceSetContainer>().named(SourceSet.MAIN_SOURCE_SET_NAME) {
+            configurations.getByName(compileClasspathConfigurationName).extendsFrom(lib)
+            configurations.getByName(runtimeClasspathConfigurationName).extendsFrom(lib)
+        }
+    }
+}
+
 val runNumber: String? = System.getenv("GITHUB_RUN_NUMBER")
 val runNumberDelimiter: String? by project
 val addRunNumber: String? by project
@@ -13,9 +23,7 @@ if (addRunNumber != "false" && runNumber != null) {
     version = "$version${runNumberDelimiter ?: '.'}$runNumber"
 }
 
-java {
-    withSourcesJar()
-}
+java.withSourcesJar()
 
 tasks.dokkaHtml {
     outputDirectory.set(file("/docs"))
@@ -30,6 +38,38 @@ publishing {
     }
     publications {
         register("maven", MavenPublication::class) {
+//            val reobfJar = tasks.findByName("reobfJar") as? RemapJar
+//            val kotlinSourcesJar = tasks.findByName("kotlinSourcesJar") as? Jar
+//            if (reobfJar != null) {
+//                artifact(reobfJar.outputJar) {
+//                    builtBy(reobfJar)
+//                    classifier = null
+//                }
+//                if (kotlinSourcesJar != null) {
+//                    artifact(kotlinSourcesJar) {
+//                        builtBy(kotlinSourcesJar)
+//                        classifier = "sources"
+//                    }
+//                }
+//                pom.withXml {
+//                    val depNode = Node(asNode(), "dependencies")
+//                    configurations["lib"].resolvedConfiguration.firstLevelModuleDependencies.forEach { dep ->
+//                        depNode.children().cast<groovy.util.NodeList>().firstOrNull() {
+//                            val node = it as Node
+//                            node["groupId"] == dep.moduleGroup &&
+//                                node["artifactId"] == dep.moduleName &&
+//                                node["version"] == dep.moduleVersion &&
+//                                node["scope"] == "provided"
+//                        } ?: run {
+//                            val node = Node(depNode, "dependency")
+//                            node.appendNode("groupId", dep.moduleGroup)
+//                            node.appendNode("artifactId", dep.moduleName)
+//                            node.appendNode("version", dep.moduleVersion)
+//                            node.appendNode("scope", "provided")
+//                        }
+//                    }
+//                }
+//            } else
             from(components[publishComponentName ?: "java"])
         }
     }
