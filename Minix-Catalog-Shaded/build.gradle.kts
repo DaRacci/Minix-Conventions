0950f9435afd7d9f5ee34a00d64785c5b9abf300
Racci <racci@racci.dev>
2022-03-26 21:00:07 +1100

    chore: Fix up the shade platform

--- Minix-Catalog-Shaded/build.gradle.kts
+++ Minix-Catalog-Shaded/build.gradle.kts
@@ -21,37 +21,31 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.adventure.api)
-    implementation(libs.adventure.kotlin)
-    implementation(libs.adventure.minimessage)
+    implementation(libs.bundles.kyori)
+    implementation(libs.adventure.platform.bukkit)
+
+    implementation(libs.bundles.cloud)
+    implementation(libs.bundles.cloud.kotlin)
+
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.kotlinx)
+    implementation(libs.bundles.exposed)
+
+    implementation(libs.cloud.minecraft.brigadier)
+    implementation(libs.cloud.minecraft.bukkit)
+    implementation(libs.cloud.minecraft.bungee)
+    implementation(libs.cloud.minecraft.cloudburst)
+    implementation(libs.cloud.minecraft.extras)
+    implementation(libs.cloud.minecraft.paper)
+    implementation(libs.cloud.minecraft.sponge7)
+    implementation(libs.cloud.minecraft.velocity)
 
     implementation(libs.minecraft.authLib)
     implementation(libs.minecraft.commandAPI)
     implementation(libs.minecraft.inventoryFramework)
     implementation(libs.minecraft.bstats)
 
-    implementation(libs.minecraft.api.floodgate)
-    implementation(libs.minecraft.api.protoclLib)
-    implementation(libs.minecraft.api.placeholderAPI)
-    implementation(libs.minecraft.api.landsAPI)
-
-    implementation(libs.exposed.core)
-    implementation(libs.exposed.dao)
-    implementation(libs.exposed.dateTime)
-    implementation(libs.hikariCP)
-
-    implementation(libs.valiktor)
-    implementation(libs.mordant)
-    implementation(libs.caffeine)
-    implementation(libs.kotlin.statistics)
-
-    implementation(libs.kotlin.stdlib)
-    implementation(libs.kotlin.reflect)
-
-    implementation(libs.kotlinx.coroutines)
-    implementation(libs.kotlinx.dateTime)
-    implementation(libs.kotlinx.immutableCollections)
-
+    implementation(libs.kotlinx.serialization.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.serialization.cbor)
     implementation(libs.kotlinx.serialization.hocon)
@@ -59,12 +53,46 @@ dependencies {
     implementation(libs.kotlinx.serialization.properties)
     implementation(libs.kotlinx.serialization.kaml)
 
+    implementation(libs.kotlin.statistics)
+    implementation(libs.valiktor)
+    implementation(libs.mordant)
+    implementation(libs.caffeine)
+
     implementation(libs.koin.core)
     implementation(libs.koin.ktor)
 
-    implementation(libs.logging.sentry)
     implementation(libs.logging.slf4JAPI)
     implementation(libs.logging.kotlinLogging)
+
+    implementation(libs.sentry.core)
+    implementation(libs.sentry.kotlin)
+
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.jetty)
+    implementation(libs.ktor.server.netty)
+
+    implementation(libs.ktor.server.plugins.auth)
+    implementation(libs.ktor.server.plugins.websockets)
+    implementation(libs.ktor.server.plugins.sessions)
+    implementation(libs.ktor.server.plugins.defaultHeaders)
+    implementation(libs.ktor.server.plugins.callLogging)
+    implementation(libs.ktor.server.plugins.locations)
+    implementation(libs.ktor.server.plugins.contentNegotiation)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.java)
+    implementation(libs.ktor.client.jetty)
+    implementation(libs.ktor.client.okhttp)
+
+    implementation(libs.ktor.client.plugins.auth)
+    implementation(libs.ktor.client.plugins.encoding)
+    implementation(libs.ktor.client.plugins.gson)
+    implementation(libs.ktor.client.plugins.serialization)
+    implementation(libs.ktor.client.plugins.logging)
+    implementation(libs.ktor.client.plugins.websockets)
 }
 
 tasks {
