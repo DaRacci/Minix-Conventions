11001d606ff15f546119b04dc1b1c22898d054fb
DaRacci <tangentmoons@gmail.com>
2022-04-05 00:40:13 +1000

    chore(deps): Update dependency io.kotest:kotest to v5.2.2

--- Minix-Gradle/src/main/kotlin/dev.racci.minix.testing.gradle.kts
+++ Minix-Gradle/src/main/kotlin/dev.racci.minix.testing.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.kotest:kotest-runner-junit5:5.2.1")
-    testImplementation("io.kotest:kotest-property:5.2.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
+    testImplementation("io.kotest:kotest-property:5.2.2")
     testImplementation("io.mockk:mockk:1.12.3")
     testImplementation("io.insert-koin:koin-test:3.1.5")
     testImplementation("io.insert-koin:koin-test-junit5:3.1.5")
