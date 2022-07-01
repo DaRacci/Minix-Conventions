83c25574692536dda003e9e4332702472f2de08e
DaRacci <racci@racci.dev>
2022-07-01 12:06:39 +1000

    chore(deps): Update LandsAPI to 6.5.1

--- Minix-NMS/src/main/kotlin/dev/racci/minix/nms/aliases/Other.kt
+++ Minix-NMS/src/main/kotlin/dev/racci/minix/nms/aliases/Other.kt
@@ -2,14 +2,11 @@
 
 package dev.racci.minix.nms.aliases
 
-import net.minecraft.network.chat.TextComponent
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.level.GameRules
 
 typealias NMSInteractionHand = InteractionHand
 typealias NMSInteractionResult = InteractionResult
-
-typealias NMSChatMessage = TextComponent
 
 typealias NMSGameRules = GameRules
