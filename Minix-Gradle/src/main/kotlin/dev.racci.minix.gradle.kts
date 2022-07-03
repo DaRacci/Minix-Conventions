d074d9dc3e0d688e840ffbd7aa055bc797d941bf
DaRacci <racci@racci.dev>
2022-07-02 22:55:16 -0700

    fix: kotlin path

--- Minix-Gradle/src/main/kotlin/dev.racci.minix.gradle.kts
+++ Minix-Gradle/src/main/kotlin/dev.racci.minix.gradle.kts
@@ -11,10 +11,10 @@ tasks {
     val relocateDependencies by creating {
         val target = getByName("shadowJar", ShadowJar::class)
         val prefix = "dev.racci.minix.libs"
-        target.relocate("org.bstats", "$prefix.org.bstats")
+        target.relocate("kotlin", "$prefix.kotlin")
+        target.relocate("kotlinx", "$prefix.kotlinx")
         target.relocate("io.sentry", "$prefix.io.sentry")
-        target.relocate("org.jetbrains.kotlin", "$prefix.org.jetbrains.kotlin")
-        target.relocate("org.jetbrains.kotlinx", "$prefix.org.jetbrains.kotlinx")
+        target.relocate("org.bstats", "$prefix.org.bstats")
         target.relocate("org.jetbrains.exposed", "$prefix.org.jetbrains.exposed")
         target.relocate("com.github.benmanes.caffeine", "$prefix.com.github.benmanes.caffeine")
     }
