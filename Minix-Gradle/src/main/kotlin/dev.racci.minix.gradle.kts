cb2e04c5755634c6f7ab434e65e9584fd0662119
DaRacci <racci@racci.dev>
2022-07-02 23:22:50 -0700

    fix: anything else that used kotlin

--- Minix-Gradle/src/main/kotlin/dev.racci.minix.gradle.kts
+++ Minix-Gradle/src/main/kotlin/dev.racci.minix.gradle.kts
@@ -11,12 +11,18 @@ tasks {
     val relocateDependencies by creating {
         val target = getByName("shadowJar", ShadowJar::class)
         val prefix = "dev.racci.minix.libs"
+
         target.relocate("kotlin", "$prefix.kotlin")
         target.relocate("kotlinx", "$prefix.kotlinx")
+        target.relocate("io.ktor", "$prefix.io.ktor")
+        target.relocate("io.koin", "$prefix.io.koin")
         target.relocate("io.sentry", "$prefix.io.sentry")
         target.relocate("org.bstats", "$prefix.org.bstats")
         target.relocate("org.jetbrains.exposed", "$prefix.org.jetbrains.exposed")
         target.relocate("com.github.benmanes.caffeine", "$prefix.com.github.benmanes.caffeine")
+        target.relocate("cloud.commandframework.kotlin", "$prefix.cloud.commandframework.kotlin")
+        target.relocate("net.kyori.adventure.extra.kotlin", "$prefix.net.kyori.adventure.extra.kotlin")
+        target.relocate("org.spongepowered.configurate.kotlin", "$prefix.org.spongepowered.configurate.kotlin")
     }
 
     getByName("shadowJar", ShadowJar::class).dependsOn(relocateDependencies)
