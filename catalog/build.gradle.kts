2d4312a57e28ce2ad33d11b88ba5a4a8047e629b
DaRacci <racci@racci.dev>
2022-12-22 22:17:14 +1100

    feat(catalog): add minix-plugin to catalog

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 plugins {
     `version-catalog`
     id("dev.racci.minix.publication")
@@ -7,18 +9,26 @@ minixPublishing.publishComponentName = "versionCatalog"
 
 catalog {
     versionCatalog {
-        version("version", version.toString())
+        version("minix-scripts", version.toString())
 
         fun minixPlugin(file: File) {
             val removedExtension = file.name.removeSuffix(".gradle.kts")
             val scriptName = removedExtension.substringAfter("dev.racci.minix.")
 
-            plugin("minix-$scriptName", removedExtension).version(version.toString())
+            plugin("minix-$scriptName", removedExtension).versionRef("minix-scripts")
         }
 
         gradle.includedBuilds.find { it.name == "Minix-Gradle" }!!.projectDir.resolve("src/main/kotlin").listFiles { file ->
             file.isFile
         }?.forEach { file -> minixPlugin(file) }
+
+        gradle.includedBuilds.find { it.name == "minix-plugin" }!!.also { gradlePlugin ->
+            val properties = loadProperties(gradlePlugin.projectDir.resolve("gradle.properties").absolutePath)
+            val version = properties["version"] as String
+
+            version("minix-plugin", version)
+            plugin("minix", "dev.racci.minix").versionRef("minix-plugin")
+        }
 
         from(files("../gradle/libs.versions.toml"))
     }
