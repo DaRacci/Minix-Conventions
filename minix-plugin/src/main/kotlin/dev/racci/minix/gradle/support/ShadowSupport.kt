cb3f855d555e6c632feb07545434a9f395173de8
DaRacci <racci@racci.dev>
2023-01-16 17:42:11 +1100

    test(plugin): use assemble instead of build

--- minix-plugin/src/main/kotlin/dev/racci/minix/gradle/support/ShadowSupport.kt
+++ minix-plugin/src/main/kotlin/dev/racci/minix/gradle/support/ShadowSupport.kt
@@ -1,6 +1,5 @@
 package dev.racci.minix.gradle.support
 
-import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import dev.racci.minix.gradle.ex.disambiguateName
 import dev.racci.minix.gradle.tasks.ShadowJarMPPTask
