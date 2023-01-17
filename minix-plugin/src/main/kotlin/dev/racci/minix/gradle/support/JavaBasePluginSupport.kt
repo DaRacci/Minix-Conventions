065d5c8d9e3f68a6c243b771faacbd19f534cda1
DaRacci <racci@racci.dev>
2023-01-17 13:42:10 +1100

    refactor(plugin:java): Use base plugin instead

--- minix-plugin/src/main/kotlin/dev/racci/minix/gradle/support/JavaBasePluginSupport.kt
+++ minix-plugin/src/main/kotlin/dev/racci/minix/gradle/support/JavaBasePluginSupport.kt
@@ -2,14 +2,14 @@ package dev.racci.minix.gradle.support
 
 import dev.racci.minix.gradle.Constants
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.configure
 
-public object JavaPluginSupport : PluginSupport(
-    "java",
-    { JavaPlugin::class }
+public object JavaBasePluginSupport : PluginSupport(
+    "java-base",
+    { JavaBasePlugin::class }
 ) {
     override fun configureSub(project: Project): Unit = project.extensions.configure<JavaPluginExtension> {
         toolchain.languageVersion.set(JavaLanguageVersion.of(Constants.JDK_VERSION))
